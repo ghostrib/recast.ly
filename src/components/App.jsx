ac6a110b74541c5b42cd26b54c615a48947c74a3
ghostrib <sh4rkbot@gmail.com>
2018-11-27 12:34:47 -0600

    (feat) Add video data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,58 @@
 import Search from './Search.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+console.log(exampleVideoData);
 
-var App = () => (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em><Search /></h5></div>
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em><Search /></h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>VideoPlayer</em> <VideoPlayer
+              video={'https://youtube.com/embed/' + exampleVideoData[1].id.video + '?autoplay=1'}
+              videoTitle={exampleVideoData[1].snippet.title}
+              videoDescription={exampleVideoData[1].snippet.description}
+            /></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em> <VideoList
+              videoData={exampleVideoData}
+            /></h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>VideoPlayer</em> <VideoPlayer /></h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> <VideoList /></h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+// var App = () => (
+
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em><Search /></h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><em>VideoPlayer</em> <VideoPlayer /></h5></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><h5><em>videoList</em> <VideoList /></h5></div>
+//       </div>
+//     </div>
+//   </div>
+// );
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
